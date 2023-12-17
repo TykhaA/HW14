3ab05548476da6dabffb88c0dc3296181f4c403c
Anastasiia Tykha <anastasiia.tykha@academysmart.com>
2023-12-17 20:00:47 +0200

    some fixes

--- js/task-04.js
+++ js/task-04.js
@@ -2,7 +2,7 @@ const counterValue = document.querySelector('#value');
 let value = 0;
 const btn = document.querySelectorAll('button');
 btn.forEach(element => element.addEventListener("click", () =>{
-    if (element.hasAttribute('decrement')){
+    if (element.getAttribute('data-action') === 'decrement'){
         value -= 1;
     } else {
          value += 1;
